b04a74b8d5b8d73b7276a3e55122d59bd5ed7622
Atlantic IT Solutions <info@atlanticitsolutions.com>
2024-02-28 15:11:39 -0500

    Certificates added

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+
+export default function Certificates() {
+  return (
+    <>
+      <ul className='list-disc list-inside py-4 space-y-2 sm:space-y-3'>
+        <li>
+          <a
+            href='https://www.credly.com/badges/f80d143c-29b9-4f48-bf47-09583db4c406/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Administrator Associate'
+          >
+            [AZ-104] &#8211; Microsoft Certified: Azure Administrator Associate
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/e3975d6c-908d-462b-b18a-c2769b0df955/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Solutions Architect Expert'
+          >
+            [AZ-305] &#8211; Microsoft Certified: Azure Solutions Architect
+            Expert
+          </a>
+          , Exam badge:{' '}
+          <a
+            href='https://www.credly.com/badges/96213a44-2982-4d30-94a4-4985e9f3c4ad/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Administrator Associate'
+          >
+            here
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/6f8ad368-f87a-4862-b9ea-dde8155afc42/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: DevOps Engineer Expert'
+          >
+            [AZ-400] &#8211; Microsoft Certified: DevOps Engineer Expert
+          </a>
+          , Exam badge:{' '}
+          <a
+            href='https://www.credly.com/badges/415c1c10-1942-416e-8bcf-d9ec3acb9555/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Solutions Architect Expert'
+          >
+            here
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/41097817-7c62-4fb6-aaa0-59237a260ba7/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Security Engineer Associate'
+          >
+            [AZ-500] &#8211; Microsoft Certified: Azure Security Engineer
+            Associate
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/911ba0f3-b2fe-4f59-b4b7-83ba8d440d67/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Fundamentals'
+          >
+            [AZ-900] &#8211; Microsoft Certified: Azure Fundamentals
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/a4df5f84-17e1-4a6b-9007-741fdf7420e6/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Azure Data Fundamentals'
+          >
+            [DP-900] &#8211; Microsoft Certified: Azure Data Fundamentals
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.credly.com/badges/8a13e27f-b78c-4f20-87b6-1659666db37d/public_url'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Microsoft Certified: Security, Compliance, and Identity Fundamentals'
+          >
+            [SC-900] &#8211; Microsoft Certified: Security, Compliance, and
+            Identity Fundamentals
+          </a>
+        </li>
+        <li>
+          Click{' '}
+          <a
+            href='https://www.credly.com/users/bilal-gonen.6e475eab/badges'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Bilal Gonen Microsoft Certificates '
+          >
+            HERE
+          </a>{' '}
+          to see all of the above certificates on credly.com website.
+        </li>
+      </ul>
+    </>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Section from '../components/Section'
 import Experience from '../components/Experience'
+import Certificates from '../components/Certificates'
 
 export const Home = () => {
   return (
-    <main className='flex flex-col p-2'>
+    <main className='flex flex-col p-2 gap-8'>
       <Section title='Experience'>
         <Experience />
+      </Section>
+      <Section title='Certificates'>
+        <Certificates />
       </Section>
     </main>
   )
